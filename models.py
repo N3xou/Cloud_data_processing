5869b2a8edaa611af4898e477415e6258eb1e15f
iYami <santox25@gmail.com>
2025-10-27 21:20:37 +0100

    dockerv.0

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Prediction(Base):
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String, index=True)
     prediction = Column(String)
-    probabilities = Column(String)  # Stored as JSON string
+    probabilities = Column(String)
     confidence = Column(Float)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
