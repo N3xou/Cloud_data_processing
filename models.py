7cce87faf43e6063c641c2aa0113d437f0dada51
iYami <santox25@gmail.com>
2025-11-11 18:23:57 +0100

    missing import

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, Text, Boolean
 from sqlalchemy.sql import func
 from db import Base
 
